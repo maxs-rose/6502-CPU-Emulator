36295d8a75b8ffaa6af489613552fb243444b8dd
Max <max.rose366@gmail.com>
2021-02-06 14:56:45 +0000

    cleaned up tests

--- Test6502/TestLoad.cs
+++ Test6502/TestLoad.cs
@@ -19,8 +19,8 @@ namespace Test_6504
         [Test]
         public void LoadImmediatePositive()
         {
-            cpu.memory[0x0100] = (byte)OpCode.LDAI;
-            cpu.memory[0x0101] = 0x37;
+            cpu[0x0100] = (byte)OpCode.LDAI;
+            cpu[0x0101] = 0x37;
 
             var cycles = 2;
             cpu.RunProgram(ref cycles);
@@ -31,10 +31,24 @@ namespace Test_6504
         }
         
         [Test]
+        public void LoadImmediateZero()
+        {
+            cpu[0x0100] = (byte)OpCode.LDAI;
+            cpu[0x0101] = 0x0;
+
+            var cycles = 2;
+            cpu.RunProgram(ref cycles);
+            Assert.AreEqual(0, cycles);
+            Assert.AreEqual(0x0, cpu.A);
+            Assert.AreEqual(Flags.Z, cpu.flags);
+            Assert.AreEqual(0x0102, cpu.pc);
+        }
+        
+        [Test]
         public void LoadImmediateNegative()
         {
-            cpu.memory[0x0100] = (byte)OpCode.LDAI;
-            cpu.memory[0x0101] = 0x80;
+            cpu[0x0100] = (byte)OpCode.LDAI;
+            cpu[0x0101] = 0x80;
 
             var cycles = 2;
             cpu.RunProgram(ref cycles);
@@ -47,9 +61,9 @@ namespace Test_6504
         [Test]
         public void LoadZPPositive()
         {
-            cpu.memory[0x0100] = (byte)OpCode.LDAZ;
-            cpu.memory[0x0101] = 0x69;
-            cpu.memory[0x0069] = 0x37;
+            cpu[0x0100] = (byte)OpCode.LDAZ;
+            cpu[0x0101] = 0x69;
+            cpu[0x0069] = 0x37;
 
             var cycles = 3;
             cpu.RunProgram(ref cycles);
@@ -62,9 +76,9 @@ namespace Test_6504
         [Test]
         public void LoadZPNegative()
         {
-            cpu.memory[0x0100] = (byte)OpCode.LDAZ;
-            cpu.memory[0x0101] = 0x69;
-            cpu.memory[0x0069] = 0x80;
+            cpu[0x0100] = (byte)OpCode.LDAZ;
+            cpu[0x0101] = 0x69;
+            cpu[0x0069] = 0x80;
 
             var cycles = 3;
             cpu.RunProgram(ref cycles);
@@ -75,12 +89,27 @@ namespace Test_6504
         }
         
         [Test]
+        public void LoadZPZero()
+        {
+            cpu[0x0100] = (byte)OpCode.LDAZ;
+            cpu[0x0101] = 0x69;
+            cpu[0x0069] = 0x0;
+
+            var cycles = 3;
+            cpu.RunProgram(ref cycles);
+            Assert.AreEqual(0, cycles);
+            Assert.AreEqual(0x0, cpu.A);
+            Assert.AreEqual(Flags.Z, cpu.flags);
+            Assert.AreEqual(0x0102, cpu.pc);
+        }
+        
+        [Test]
         public void LoadZPXPositive()
         {
             cpu.X = 1;
-            cpu.memory[0x0100] = (byte)OpCode.LDAZX;
-            cpu.memory[0x0101] = 0x69;
-            cpu.memory[0x006A] = 0x37;
+            cpu[0x0100] = (byte)OpCode.LDAZX;
+            cpu[0x0101] = 0x69;
+            cpu[0x006A] = 0x37;
 
             var cycles = 4;
             cpu.RunProgram(ref cycles);
@@ -94,9 +123,9 @@ namespace Test_6504
         public void LoadZPXNegative()
         {
             cpu.X = 1;
-            cpu.memory[0x0100] = (byte)OpCode.LDAZX;
-            cpu.memory[0x0101] = 0x69;
-            cpu.memory[0x006A] = 0x80;
+            cpu[0x0100] = (byte)OpCode.LDAZX;
+            cpu[0x0101] = 0x69;
+            cpu[0x006A] = 0x80;
 
             var cycles = 4;
             cpu.RunProgram(ref cycles);
@@ -107,12 +136,28 @@ namespace Test_6504
         }
         
         [Test]
+        public void LoadZPXZero()
+        {
+            cpu.X = 1;
+            cpu[0x0100] = (byte)OpCode.LDAZX;
+            cpu[0x0101] = 0x69;
+            cpu[0x006A] = 0x0;
+
+            var cycles = 4;
+            cpu.RunProgram(ref cycles);
+            Assert.AreEqual(0, cycles);
+            Assert.AreEqual(0x0, cpu.A);
+            Assert.AreEqual(Flags.Z, cpu.flags);
+            Assert.AreEqual(0x0102, cpu.pc);
+        }
+        
+        [Test]
         public void LoadAbsPositive()
         {
-            cpu.memory[0x0100] = (byte)OpCode.LDAA;
-            cpu.memory[0x0101] = 0x69;
-            cpu.memory[0x0102] = 0x37;
-            cpu.memory[0x3769] = 0x37;
+            cpu[0x0100] = (byte)OpCode.LDAA;
+            cpu[0x0101] = 0x69;
+            cpu[0x0102] = 0x37;
+            cpu[0x3769] = 0x37;
 
             var cycles = 4;
             cpu.RunProgram(ref cycles);
@@ -125,10 +170,10 @@ namespace Test_6504
         [Test]
         public void LoadAbsNegative()
         {
-            cpu.memory[0x0100] = (byte)OpCode.LDAA;
-            cpu.memory[0x0101] = 0x69;
-            cpu.memory[0x0102] = 0x37;
-            cpu.memory[0x3769] = 0x80;
+            cpu[0x0100] = (byte)OpCode.LDAA;
+            cpu[0x0101] = 0x69;
+            cpu[0x0102] = 0x37;
+            cpu[0x3769] = 0x80;
 
             var cycles = 4;
             cpu.RunProgram(ref cycles);
@@ -139,14 +184,30 @@ namespace Test_6504
         }
         
         [Test]
+        public void LoadAbsZero()
+        {
+            cpu[0x0100] = (byte)OpCode.LDAA;
+            cpu[0x0101] = 0x69;
+            cpu[0x0102] = 0x37;
+            cpu[0x3769] = 0x00;
+
+            var cycles = 4;
+            cpu.RunProgram(ref cycles);
+            Assert.AreEqual(0, cycles);
+            Assert.AreEqual(0x00, cpu.A);
+            Assert.AreEqual(Flags.Z, cpu.flags);
+            Assert.AreEqual(0x0103, cpu.pc);
+        }
+        
+        [Test]
         public void LoadAbsXPositive()
         {
             cpu.X = 1;
             
-            cpu.memory[0x0100] = (byte)OpCode.LDAAX;
-            cpu.memory[0x0101] = 0x69;
-            cpu.memory[0x0102] = 0x37;
-            cpu.memory[0x376A] = 0x37;
+            cpu[0x0100] = (byte)OpCode.LDAAX;
+            cpu[0x0101] = 0x69;
+            cpu[0x0102] = 0x37;
+            cpu[0x376A] = 0x37;
 
             var cycles = 4;
             cpu.RunProgram(ref cycles);
@@ -161,10 +222,10 @@ namespace Test_6504
         {
             cpu.X = 1;
             
-            cpu.memory[0x0100] = (byte)OpCode.LDAAX;
-            cpu.memory[0x0101] = 0xFF;
-            cpu.memory[0x0102] = 0x01;
-            cpu.memory[0x0200] = 0x37;
+            cpu[0x0100] = (byte)OpCode.LDAAX;
+            cpu[0x0101] = 0xFF;
+            cpu[0x0102] = 0x01;
+            cpu[0x0200] = 0x37;
 
             var cycles = 5;
             cpu.RunProgram(ref cycles);
@@ -179,10 +240,10 @@ namespace Test_6504
         {
             cpu.X = 1;
 
-            cpu.memory[0x0100] = (byte)OpCode.LDAAX;
-            cpu.memory[0x0101] = 0x69;
-            cpu.memory[0x0102] = 0x37;
-            cpu.memory[0x376A] = 0x80;
+            cpu[0x0100] = (byte)OpCode.LDAAX;
+            cpu[0x0101] = 0x69;
+            cpu[0x0102] = 0x37;
+            cpu[0x376A] = 0x80;
 
             var cycles = 4;
             cpu.RunProgram(ref cycles);
@@ -197,10 +258,10 @@ namespace Test_6504
         {
             cpu.X = 1;
             
-            cpu.memory[0x0100] = (byte)OpCode.LDAAX;
-            cpu.memory[0x0101] = 0xFF;
-            cpu.memory[0x0102] = 0x01;
-            cpu.memory[0x0200] = 0x80;
+            cpu[0x0100] = (byte)OpCode.LDAAX;
+            cpu[0x0101] = 0xFF;
+            cpu[0x0102] = 0x01;
+            cpu[0x0200] = 0x80;
 
             var cycles = 5;
             cpu.RunProgram(ref cycles);
@@ -211,14 +272,50 @@ namespace Test_6504
         }
         
         [Test]
+        public void LoadAbsXZero()
+        {
+            cpu.X = 1;
+
+            cpu[0x0100] = (byte)OpCode.LDAAX;
+            cpu[0x0101] = 0x69;
+            cpu[0x0102] = 0x37;
+            cpu[0x376A] = 0x80;
+
+            var cycles = 4;
+            cpu.RunProgram(ref cycles);
+            Assert.AreEqual(0, cycles);
+            Assert.AreEqual(0x80, cpu.A);
+            Assert.AreEqual(Flags.N, cpu.flags);
+            Assert.AreEqual(0x0103, cpu.pc);
+        }
+        
+        [Test]
+        public void LoadAbsXCrossBoundaryZero()
+        {
+            cpu.X = 1;
+            
+            cpu[0x0100] = (byte)OpCode.LDAAX;
+            cpu[0x0101] = 0xFF;
+            cpu[0x0102] = 0x01;
+            cpu[0x0200] = 0x00;
+
+            var cycles = 5;
+            cpu.RunProgram(ref cycles);
+            Assert.AreEqual(0, cycles);
+            Assert.AreEqual(0x00, cpu.A);
+            Assert.AreEqual(Flags.Z, cpu.flags);
+            Assert.AreEqual(0x0103, cpu.pc);
+        }
+        
+        [Test]
         public void LoadAbsYPositive()
         {
             cpu.Y = 1;
             
-            cpu.memory[0x0100] = (byte)OpCode.LDAAY;
-            cpu.memory[0x0101] = 0x69;
-            cpu.memory[0x0102] = 0x37;
-            cpu.memory[0x376A] = 0x37;
+            cpu[0x0100] = (byte)OpCode.LDAAY;
+            cpu[0x0101] = 0x69;
+            cpu[0x0102] = 0x37;
+            cpu[0x376A] = 0x37;
 
             var cycles = 4;
             cpu.RunProgram(ref cycles);
@@ -233,10 +330,10 @@ namespace Test_6504
         {
             cpu.Y = 1;
             
-            cpu.memory[0x0100] = (byte)OpCode.LDAAY;
-            cpu.memory[0x0101] = 0xFF;
-            cpu.memory[0x0102] = 0x01;
-            cpu.memory[0x0200] = 0x37;
+            cpu[0x0100] = (byte)OpCode.LDAAY;
+            cpu[0x0101] = 0xFF;
+            cpu[0x0102] = 0x01;
+            cpu[0x0200] = 0x37;
 
             var cycles = 5;
             cpu.RunProgram(ref cycles);
@@ -251,10 +348,10 @@ namespace Test_6504
         {
             cpu.Y = 1;
 
-            cpu.memory[0x0100] = (byte)OpCode.LDAAY;
-            cpu.memory[0x0101] = 0x69;
-            cpu.memory[0x0102] = 0x37;
-            cpu.memory[0x376A] = 0x80;
+            cpu[0x0100] = (byte)OpCode.LDAAY;
+            cpu[0x0101] = 0x69;
+            cpu[0x0102] = 0x37;
+            cpu[0x376A] = 0x80;
 
             var cycles = 4;
             cpu.RunProgram(ref cycles);
@@ -269,10 +366,10 @@ namespace Test_6504
         {
             cpu.Y = 1;
             
-            cpu.memory[0x0100] = (byte)OpCode.LDAAY;
-            cpu.memory[0x0101] = 0xFF;
-            cpu.memory[0x0102] = 0x01;
-            cpu.memory[0x0200] = 0x80;
+            cpu[0x0100] = (byte)OpCode.LDAAY;
+            cpu[0x0101] = 0xFF;
+            cpu[0x0102] = 0x01;
+            cpu[0x0200] = 0x80;
 
             var cycles = 5;
             cpu.RunProgram(ref cycles);
@@ -283,15 +380,51 @@ namespace Test_6504
         }
         
         [Test]
+        public void LoadAbsYZero()
+        {
+            cpu.Y = 1;
+
+            cpu[0x0100] = (byte)OpCode.LDAAY;
+            cpu[0x0101] = 0x69;
+            cpu[0x0102] = 0x37;
+            cpu[0x376A] = 0x00;
+
+            var cycles = 4;
+            cpu.RunProgram(ref cycles);
+            Assert.AreEqual(0, cycles);
+            Assert.AreEqual(0x00, cpu.A);
+            Assert.AreEqual(Flags.Z, cpu.flags);
+            Assert.AreEqual(0x0103, cpu.pc);
+        }
+        
+        [Test]
+        public void LoadAbsYCrossBoundaryZero()
+        {
+            cpu.Y = 1;
+            
+            cpu[0x0100] = (byte)OpCode.LDAAY;
+            cpu[0x0101] = 0xFF;
+            cpu[0x0102] = 0x01;
+            cpu[0x0200] = 0x00;
+
+            var cycles = 5;
+            cpu.RunProgram(ref cycles);
+            Assert.AreEqual(0, cycles);
+            Assert.AreEqual(0x00, cpu.A);
+            Assert.AreEqual(Flags.Z, cpu.flags);
+            Assert.AreEqual(0x0103, cpu.pc);
+        }
+        
+        [Test]
         public void LoadIndirectXPositive()
         {
             cpu.X = 4;
             
-            cpu.memory[0x0100] = (byte)OpCode.LDAIX;
-            cpu.memory[0x0101] = 0x02;
-            cpu.memory[0x0006] = 0x00;
-            cpu.memory[0x0007] = 0x80;
-            cpu.memory[0x8000] = 0x37;
+            cpu[0x0100] = (byte)OpCode.LDAIX;
+            cpu[0x0101] = 0x02;
+            cpu[0x0006] = 0x00;
+            cpu[0x0007] = 0x80;
+            cpu[0x8000] = 0x37;
 
             var cycles = 6;
             cpu.RunProgram(ref cycles);
@@ -306,11 +439,11 @@ namespace Test_6504
         {
             cpu.X = 4;
 
-            cpu.memory[0x0100] = (byte)OpCode.LDAIX;
-            cpu.memory[0x0101] = 0x02;
-            cpu.memory[0x0006] = 0x00;
-            cpu.memory[0x0007] = 0x80;
-            cpu.memory[0x8000] = 0x80;
+            cpu[0x0100] = (byte)OpCode.LDAIX;
+            cpu[0x0101] = 0x02;
+            cpu[0x0006] = 0x00;
+            cpu[0x0007] = 0x80;
+            cpu[0x8000] = 0x80;
 
             var cycles = 6;
             cpu.RunProgram(ref cycles);
@@ -321,15 +454,34 @@ namespace Test_6504
         }
         
         [Test]
+        public void LoadIndirectXZero()
+        {
+            cpu.X = 4;
+
+            cpu[0x0100] = (byte)OpCode.LDAIX;
+            cpu[0x0101] = 0x02;
+            cpu[0x0006] = 0x00;
+            cpu[0x0007] = 0x80;
+            cpu[0x8000] = 0x00;
+
+            var cycles = 6;
+            cpu.RunProgram(ref cycles);
+            Assert.AreEqual(0, cycles);
+            Assert.AreEqual(0x00, cpu.A);
+            Assert.AreEqual(Flags.Z, cpu.flags);
+            Assert.AreEqual(0x0102, cpu.pc);
+        }
+        
+        [Test]
         public void LoadIndirectYPositive()
         {
             cpu.Y = 4;
             
-            cpu.memory[0x0100] = (byte)OpCode.LDAIY;
-            cpu.memory[0x0101] = 0x02;
-            cpu.memory[0x0002] = 0x00;
-            cpu.memory[0x0003] = 0x80;
-            cpu.memory[0x8004] = 0x37;
+            cpu[0x0100] = (byte)OpCode.LDAIY;
+            cpu[0x0101] = 0x02;
+            cpu[0x0002] = 0x00;
+            cpu[0x0003] = 0x80;
+            cpu[0x8004] = 0x37;
 
             var cycles = 5;
             cpu.RunProgram(ref cycles);
@@ -344,11 +496,11 @@ namespace Test_6504
         {
             cpu.Y = 4;
             
-            cpu.memory[0x0100] = (byte)OpCode.LDAIY;
-            cpu.memory[0x0101] = 0x02;
-            cpu.memory[0x0002] = 0xFF;
-            cpu.memory[0x0003] = 0x00;
-            cpu.memory[0x0103] = 0x37;
+            cpu[0x0100] = (byte)OpCode.LDAIY;
+            cpu[0x0101] = 0x02;
+            cpu[0x0002] = 0xFF;
+            cpu[0x0003] = 0x00;
+            cpu[0x0103] = 0x37;
 
             var cycles = 6;
             cpu.RunProgram(ref cycles);
@@ -363,11 +515,11 @@ namespace Test_6504
         {
             cpu.Y = 4;
 
-            cpu.memory[0x0100] = (byte)OpCode.LDAIY;
-            cpu.memory[0x0101] = 0x02;
-            cpu.memory[0x0002] = 0x00;
-            cpu.memory[0x0003] = 0x80;
-            cpu.memory[0x8004] = 0x80;
+            cpu[0x0100] = (byte)OpCode.LDAIY;
+            cpu[0x0101] = 0x02;
+            cpu[0x0002] = 0x00;
+            cpu[0x0003] = 0x80;
+            cpu[0x8004] = 0x80;
 
             var cycles = 5;
             cpu.RunProgram(ref cycles);
@@ -382,17 +534,55 @@ namespace Test_6504
         {
             cpu.Y = 4;
             
-            cpu.memory[0x0100] = (byte)OpCode.LDAIY;
-            cpu.memory[0x0101] = 0x02;
-            cpu.memory[0x0002] = 0xFF;
-            cpu.memory[0x0003] = 0x00;
-            cpu.memory[0x0103] = 0x80;
+            cpu[0x0100] = (byte)OpCode.LDAIY;
+            cpu[0x0101] = 0x02;
+            cpu[0x0002] = 0xFF;
+            cpu[0x0003] = 0x00;
+            cpu[0x0103] = 0x80;
 
             var cycles = 6;
             cpu.RunProgram(ref cycles);
             Assert.AreEqual(0, cycles);
             Assert.AreEqual(0x80, cpu.A);
             Assert.AreEqual(Flags.N, cpu.flags);
+            Assert.AreEqual(0x0102, cpu.pc);
+        }
+        
+        [Test]
+        public void LoadIndirectYZero()
+        {
+            cpu.Y = 4;
+
+            cpu[0x0100] = (byte)OpCode.LDAIY;
+            cpu[0x0101] = 0x02;
+            cpu[0x0002] = 0x00;
+            cpu[0x0003] = 0x80;
+            cpu[0x8004] = 0x80;
+
+            var cycles = 5;
+            cpu.RunProgram(ref cycles);
+            Assert.AreEqual(0, cycles);
+            Assert.AreEqual(0x80, cpu.A);
+            Assert.AreEqual(Flags.N, cpu.flags);
+            Assert.AreEqual(0x0102, cpu.pc);
+        }
+        
+        [Test]
+        public void LoadIndirectYCrossBoundaryZero()
+        {
+            cpu.Y = 4;
+            
+            cpu[0x0100] = (byte)OpCode.LDAIY;
+            cpu[0x0101] = 0x02;
+            cpu[0x0002] = 0xFF;
+            cpu[0x0003] = 0x00;
+            cpu[0x0103] = 0x00;
+
+            var cycles = 6;
+            cpu.RunProgram(ref cycles);
+            Assert.AreEqual(0, cycles);
+            Assert.AreEqual(0x00, cpu.A);
+            Assert.AreEqual(Flags.Z, cpu.flags);
             Assert.AreEqual(0x0102, cpu.pc);
         }
     }
